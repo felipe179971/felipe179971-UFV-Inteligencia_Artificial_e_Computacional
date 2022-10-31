55df0160a71062aef1d1824d5790cb8fc9a35b55
Felipe da Rocha Ferreira <felipe179971@hotmail.com>
2022-10-31 12:58:47 -0300

    10/31/2022

--- ELT 572 - LINGUAGENS DE PROGRAMAÇÃO PARA INTELIGÊNCIA ARTIFICIAL/01. Aula 01--- Linguagem de Programacao para inteligencia artificial ---Python.py
+++ ELT 572 - LINGUAGENS DE PROGRAMAÇÃO PARA INTELIGÊNCIA ARTIFICIAL/01. Aula 01--- Linguagem de Programacao para inteligencia artificial ---Python.py
@@ -1,1 +1,62 @@
-2+2
+#Inteiros#######################################################################
+#Soma
+a=2;b=3;c=a+b;c
+#Subtração
+d=b-a;d
+#Multiplicação
+e=a*b;e
+#Divisão
+f=b/a;f
+#exponenciação
+g=a**b;g
+#Operações
+a=2;b=3;c=4
+a+b*c
+(a+b)*c
+a+b*c/2
+#Ponto flutuante################################################################
+a=.1;b=.2;c=a+b;c
+e=2*b;e
+a=1;b=2.0;r2=a+b;r2
+a=1;b=2;r2=a+b;r2
+##Constantes####################################################################
+MAX_VALUE=10;MAX_VALUE
+##Texto#########################################################################
+mystring1="I have a son who is 6";mystring1
+mystring2="ada lovelace";mystring2
+name="ada lovelace"
+name.title()
+name.upper()
+name.lower()
+#f-strings
+first_name="ada"
+last_name="lovelace"
+full_name=f"{first_name} {last_name}";full_name
+
+favorite_language=" python "
+#remove espaços à direita
+favorite_language.rstrip()
+#remove espaços à esquerda
+favorite_language.lstrip()
+#remove de ambos os lados
+favorite_language.strip()
+#Operadores aritméticos#########################################################
+a=9;b=4;add=a+b;add
+sub=a-b;sub
+mul=a*b;mul
+div1=a/b;div1
+div2=a//b;div2
+mod=a%b;mod
+p=a**b;p
+#Operadores relacionais#########################################################
+a=13;b=33
+a>b
+a<b
+a!=b
+a==b
+a>=b
+a<=b
+#Indentação#####################################################################
+if 5>2:
+  print("Cinco é maior que 2")
+  
