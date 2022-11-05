f941f7dc9cbc346a866c12b484a28f2db259e960
Felipe Ferreira <felipe179971@hotmail.com>
2022-11-05 13:31:15 -0300

    11/5/2022

--- ELT 572 - LINGUAGENS DE PROGRAMAÇÃO PARA INTELIGÊNCIA ARTIFICIAL/04. Aula 03--- Linguagem de Programacao para inteligencia artificial ---Python.py
+++ ELT 572 - LINGUAGENS DE PROGRAMAÇÃO PARA INTELIGÊNCIA ARTIFICIAL/04. Aula 03--- Linguagem de Programacao para inteligencia artificial ---Python.py
@@ -27,8 +27,35 @@ for i in range(0,n):
   print("%5.2f"%A[i],end=";") #Comprimento 5 e 2 casas decimais
 print()
 #Strings########################################################################
+a="Diga Olá!"
+print("Caracter de índice 1...")
+print(a[1]) #acessa elemento do string
+#percorre elemento do string 1 a 1 
+for i in a:
+  print(i,end='\n')
+#percorre string com passo 2
+print("Imprime string...")
+for i in range (0,len(a),2): #len(a) é uma função que retorna o tamanho do string a
 
-
-
-
-
+  print(a[i],end="\n")
+#percorre elemento do string 1 a 1 
+print("Imprime string...")
+for i in range (0,len(a),1):
+  print(a[i],end="\n")
+#Matrizes#######################################################################
+import numpy as np
+#le dimensoes da matriz
+nl=int(input("Informe o numero de linhas: "))
+nc=int(input("Informa o numero de colunas: "))
+#cria matriz
+A=np.empty((nl,nc))
+#le do teclado
+for i in range (0,nl):#linhas vao de 0 a nl-1
+  for j in range(0,nc): #coluans vao de 0 a nc-1
+    s='Informe o elemento A[%d][%d]: ' % (i,j)
+    A[i][j]=float(input(s))
+print("Imprime matriz...")
+for i in range(0,nl):
+  for j in range(0,nc):
+    print('%5.1f' % (A[i][j]),end='')
+  print() #imprime quebra de linha
